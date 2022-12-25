18a8d9e64477d8f184ddd98102d1edd3e35f49a4
Thomas Stivers <thomas.stivers@gmail.com>
2022-12-25 07:26:40 -0600

    Fix typo which broke CI workflow.

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ mail = Mail()
 migrate = Migrate()
 moment = Moment()
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
@@ -32,12 +33,15 @@ def create_app(config_class=Config):
     moment.init_app(app)
 
     from app.auth import bp as auth_bp
+
     app.register_blueprint(auth_bp, url_prefix="/auth")
 
     from app.errors import bp as errors_bp
+
     app.register_blueprint(errors_bp)
 
     from app.main import bp as main_bp
+
     app.register_blueprint(main_bp)
 
     if not app.debug and not app.testing:
@@ -69,15 +73,17 @@ def create_app(config_class=Config):
             maxBytes=10240,
             backupCount=10,
         )
-        file_handler.setFormatter(logging.Formatter(
-            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]",
-        ))
+        file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]",
+            )
+        )
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
-
-        appl.logger.setLevel(logger.INFO)
+        app.logger.setLevel(logger.INFO)
         app.logger.info("Poker startup")
 
     return app
+
 
 from app import models
